31dd4d4ffb40e44482b8b7f3cd11f76f8af34d59
yencute0104 <yencute0104@gmail.com>
2021-07-11 14:40:53 +0700

    push code client

---
+++ client/src/components/Auth/Input.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { TextField, Grid, InputAdornment, IconButton } from '@material-ui/core';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+
+export default function Input({name, half, handleChange, handleShowPassword, label, autoFocus, type}) {
+    return (
+        <Grid item xs ={12} sm={half ? 6:12} >
+            <TextField 
+            name ={name} 
+            onChange={handleChange} 
+            variant="outlined" 
+            required 
+            fullWidth 
+            label ={label} 
+            autoFocus={autoFocus} 
+            type={type} 
+            InputProps={name === 'password' ?{
+                endAdornment:(
+                    <InputAdornment position="end">
+                        <IconButton onClick={handleShowPassword}>
+                            {type === 'password' ? <Visibility/>: <VisibilityOff/>}
+                        </IconButton>
+                    </InputAdornment>
+                ),
+            }: null}
+            />
+        </Grid>
+    )
+};
